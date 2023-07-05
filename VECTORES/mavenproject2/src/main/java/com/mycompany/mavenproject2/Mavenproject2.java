09ddb9836f776f7d3d202ad636c2e006fde0e1c3
FerRamBCS <74386601+FerRamBCS@users.noreply.github.com>
2023-07-05 09:30:27 -0700

    TRABAJO VECTORES

---
+++ VECTORES/mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+//VECTORES 
+/**
+ *
+ * @author ASUS TUF
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        int[] vector = new int[10];
+        
+        vector[0]= 1;
+        vector[1]=25;
+        vector[2]=32;
+        vector[3]=412;
+        vector[4]=45;
+        vector[5]=556;
+        vector[6]=73;
+        vector[7]=83;
+        vector[8]=94;
+        vector[9]=10;
+        for (int i = 0; i < vector.length; i++){
+            System.out.println(vector[i]);
+        }
+    }
+}
