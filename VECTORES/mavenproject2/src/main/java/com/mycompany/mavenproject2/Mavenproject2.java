0589569f550f7d41287233b87f3373b23a9a508f
FerRamBCS <74386601+FerRamBCS@users.noreply.github.com>
2023-07-05 10:01:56 -0700

    ACTIVIDAD VECTOR
    Vector pero el usuario puede ingresar la longitud del vector

--- VECTORES/mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ VECTORES/mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -1,30 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject2;
-//VECTORES 
-/**
- *
- * @author ASUS TUF
- */
+//VECTORES POR: JOSE FERNANDO RAMIREZ BRAVO
+import java.util.Scanner;
 public class Mavenproject2 {
 
     public static void main(String[] args) {
-        int[] vector = new int[10];
+        Scanner sc = new Scanner (System.in);
+        while(true){
+         menu();
+        int longitud = sc.nextInt();
+        int [] vector = new int[longitud];
         
-        vector[0]= 1;
-        vector[1]=25;
-        vector[2]=32;
-        vector[3]=412;
-        vector[4]=45;
-        vector[5]=556;
-        vector[6]=73;
-        vector[7]=83;
-        vector[8]=94;
-        vector[9]=10;
-        for (int i = 0; i < vector.length; i++){
+        System.out.println("Ingrese los datos al vector: \n");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            vector[i] = sc.nextInt();
+        }
+        System.out.println("Los datos guardadoes en el vector son: \n");
+        for(int i = 0; i< vector.length; i++){
             System.out.println(vector[i]);
         }
+       }
+     
+    }
+    public static void menu(){
+        
+        System.out.println("-----------------------------------------------\n");
+        System.out.println("                  VECTORES                     \n");
+        System.out.println("-----------------------------------------------\n");
+        System.out.println("Ingrese la cantidad de espacios en el vector: ");
     }
 }
